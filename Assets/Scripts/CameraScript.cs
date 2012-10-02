1b87c92265f354a5ca085e8903110421b5977945
Skybladev2 <qlzp2@mail.ru>
2012-10-02 22:00:41 +0400

    Made pinch zoom more stable, but still centered.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,6 +11,12 @@ public class CameraScript : MonoBehaviour {
 	private Vector3 initialMousePos;
 
 	private bool drag = false;
+	private bool zoom = false;
+
+	private Vector2 initialTouch0Position;
+	private Vector2 initialTouch1Position;
+	private float initialOrthographicSize = 1;
+
 	public GUIText touchLabel;
 	public GUIText transformLabel;
 	public GUIText positionLabel;
@@ -19,8 +25,9 @@ public class CameraScript : MonoBehaviour {
 	public GUIText pinchDeltaLabel;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+
 	}
 	
 	// Update is called once per frame
@@ -118,28 +125,44 @@ public class CameraScript : MonoBehaviour {
 
 		if(Input.touchCount == 2)
 		{
+			drag = false;
+
 			Touch touch0 = Input.GetTouch(0);			
 			Touch touch1 = Input.GetTouch(1);
-			float scaleFactor = GetScaleFactor(touch0.position, touch1.position, touch0.deltaPosition, touch1.deltaPosition);
-			Vector2 translationDelta = GetTranslationDelta(touch0.position, touch0.position,
-			                                               touch0.deltaPosition, touch1.deltaPosition,
-			                                               this.transform.position, scaleFactor);
+
+			if(!zoom)
+			{
+				initialTouch0Position = touch0.position;
+				initialTouch1Position = touch1.position;
+				initialOrthographicSize = Camera.main.orthographicSize;
+
+				zoom = true;
+			}
+			else
+			{
+				float delta0 = Vector2.Distance(touch0.position, initialTouch0Position);
+				float delta1 = Vector2.Distance(touch1.position, initialTouch1Position);
+				float scaleFactor = GetScaleFactor(touch0.position, 
+				                                   touch1.position, 
+				                                   initialTouch0Position, 
+				                                   initialTouch1Position);
+				//Vector2 translationDelta = GetTranslationDelta(touch0.position, touch0.position,
+				//                                               touch0.deltaPosition, touch1.deltaPosition,
+				//                                               this.transform.position, scaleFactor);
 
 
-			this.pinchScaleLabel.text = scaleFactor.ToString();
-			this.pinchDeltaLabel.text = translationDelta.ToString();
-			Camera.main.orthographicSize /= scaleFactor;
+				this.pinchScaleLabel.text = scaleFactor.ToString();
+				//this.pinchDeltaLabel.text = translationDelta.ToString();
+				Camera.main.orthographicSize = initialOrthographicSize / scaleFactor;
+			}
 		}
 //#endif
 
 
 	}
 
-	public static float GetScaleFactor(Vector2 position1, Vector2 position2, Vector2 delta1, Vector2 delta2)
+	public static float GetScaleFactor(Vector2 position1, Vector2 position2, Vector2 oldPosition1, Vector2 oldPosition2)
 	{
-		Vector2 oldPosition1 = position1 - delta1;
-		Vector2 oldPosition2 = position2 - delta2;
-		
 		float distance = Vector2.Distance(position1, position2);
 		float oldDistance = Vector2.Distance(oldPosition1, oldPosition2);
 		
